b70e3c324c4704b185e1f682cd9add254f17b3ef
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-11-30 21:23:16 +0500

    feat[main]: add logging

--- app/main.py
+++ app/main.py
@@ -1,34 +1,41 @@
 from fastapi import FastAPI
 
-from app.core.config import settings
-from app.api.v1.endpoints.files import router_v1
+from app.core.config import settings_v1
+from app.v1.api.endpoints.files import router_v1
 from app.db.session import Base, engine
+from app.core.setup_logging import logger_v1
 
 
 def create_app() -> FastAPI:
     """Create and configure a FastAPI application.
 
-    `Description`: This function initializes the application settings,
-    checks if the storage exists, creates all database tables,
-    and includes the API router for version 1.
+    `Description`:
+        This function initializes the application settings,
+        checks if the storage exists, creates all database tables,
+        and and mounts API versions as sub-applications.
 
     `Returns`:
-        FastAPI: An instance of the FastAPI application
-        configured with application settings.
+        **FastAPI**: Main FastAPI application instance with all versions.
     """
-    settings.is_storage_exist()
-    Base.metadata.create_all(bind=engine)
-    app = FastAPI(
-        title=settings.APP_TITLE,
-        version=settings.APP_VERSION,
-        description=settings.APP_DESCIPTION
-    )
-    app.include_router(
-        router_v1,
-        prefix=settings.ROUTE_PREFIX,
-        tags=['Files']
-    )
-    return app
+    try:
+        logger_v1.debug('The application is launched...')
+        Base.metadata.create_all(bind=engine)
+        app = FastAPI(
+            title=settings_v1.APP_TITLE,
+            version=settings_v1.APP_VERSION,
+            description=settings_v1.APP_DESCRIPTION,
+            docs_url=settings_v1.DOCS_URL,
+            redoc_url=settings_v1.REDOC_URL,
+        )
+        logger_v1.debug(f'App: {app.title}[{app.version}] is setup.')
+        app.include_router(
+            router_v1,
+            prefix=settings_v1.ROUTE_PREFIX,
+            tags=['Files']
+        )
+        return app
+    except Exception as err:
+        logger_v1.critical(f'{err}', exc_info=True)
 
 
 app = create_app()
