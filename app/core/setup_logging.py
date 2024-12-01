72f67d17870af09770749a954f2969dc0c3db699
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-12-01 14:36:40 +0500

    feat[core]: add sqlalchemy logging setup func

--- app/core/setup_logging.py
+++ app/core/setup_logging.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 from app.core.config import settings_v1, Settings
 
 
-def setup(settings: Settings):
+def setup_app_logging(settings: Settings):
     """Configuring logging based on the settings passed.
 
     `Args`:
@@ -32,4 +32,33 @@ def setup(settings: Settings):
     return logger
 
 
-logger_v1 = setup(settings_v1)
+def setup_sqlalchemy_logging(settings: Settings):
+    """Configuring SQLAlchemy logging based on the settings passed.
+
+    `Args`:
+        **setting (Settings)**: App configuration object
+    """
+    sa_log_file = os.path.join(settings.LOG_DIR, settings.LOG_FILE_SQLALCHEMY)
+    handler = RotatingFileHandler(
+        sa_log_file,
+        maxBytes=settings.MAX_LOG_FILE_SIZE,
+        backupCount=settings.BACKUP_COUNT,
+    )
+    formatter = logging.Formatter(
+        '[%(asctime)s][%(levelname)s][%(message)s]'
+    )
+    handler.setFormatter(formatter)
+
+    sa_logger = logging.getLogger('sqlalchemy')
+    sa_logger.setLevel(settings.SQLALCHEMY_LOG_LEVEL)
+    sa_logger.addHandler(handler)
+    sa_logger.propagate = False
+
+    if settings.SQLALCHEMY_ECHO:
+        sa_logger.info('SQLAlchemy echo mode: Enabled')
+    else:
+        sa_logger.info('SQLAlchemy echo mode: Disabled')
+
+
+logger_v1 = setup_app_logging(settings_v1)
+setup_sqlalchemy_logging(settings_v1)
